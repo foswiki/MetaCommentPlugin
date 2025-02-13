686c7357adf6375e8e5b2dcb533c3202ef95e520
MichaelDaum <daum@michaeldaumconsulting.com>
2025-02-13 19:03:28 +0100

    Item15473: improve ux of comment dialog

--- lib/Foswiki/Plugins/MetaCommentPlugin.pm
+++ lib/Foswiki/Plugins/MetaCommentPlugin.pm
@@ -20,7 +20,7 @@ use Foswiki::Plugins ();
 use Foswiki::Contrib::JsonRpcContrib ();
 use Foswiki::Plugins::JQueryPlugin ();
 
-our $VERSION = '9.20';
+our $VERSION = '9.21';
 our $RELEASE = '%$RELEASE%';
 our $SHORTDESCRIPTION = 'An easy to use comment system';
 our $LICENSECODE = '%$LICENSECODE%';
